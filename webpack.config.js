568a4e758b422ff71edb7150a7e04295590bc156
Evgeny Golovin <yokopoomy20@gmail.com>
2023-09-27 22:06:23 +0300

    Modules_Webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+	entry: './src/index.js',
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					MiniCSSExtractPlugin.loader,
+					'css-loader'
+				]
+			},
+			{
+				test: /\.js$/,
+				exclude: /index.js$/,
+				loader: 'babel-loader'
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			filename: 'index.html'
+		}),
+		new MiniCSSExtractPlugin()
+	]
+}
